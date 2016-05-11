f257a63a1a54592b16b4d5e1d627c0c9597f55fe
gerdru <g_druschke@mail.com>
2016-05-10 23:27:08 -0300

    Insert hook in user model

--- models/user.js
+++ models/user.js
@@ -1,19 +1,28 @@
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
-		email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true, // makes sure the email is not used with other user
-			vaildate: {
-				isEmail: true
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true, // makes sure the email is not used with other user
+				vaildate: {
+					isEmail: true
+				}
+			},
+			password: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				vaildate: {
+					len: [7, 100]
+				}
 			}
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			vaildate: {
-				len: [7, 100]
+		}, {
+			hooks: {
+				beforeValidate: function(user, options) {
+					if (typeof user.email === 'string') {
+						user.email = user.email.toLowerCase()
+					}
+				}
 			}
 		}
-	})
+	)
 };
