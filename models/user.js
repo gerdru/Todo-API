e4cbd9eb76e2e361fca61a63da38256486d0832c
gerdru <g_druschke@mail.com>
2016-05-17 09:44:36 -0400

    hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,28 +1,50 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('user', {
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				unique: true, // makes sure the email is not used with other user
-				vaildate: {
-					isEmail: true
-				}
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				vaildate: {
-					len: [7, 100]
-				}
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true, // makes sure the email is not used with other user
+			vaildate: {
+				isEmail: true
 			}
-		}, {
-			hooks: {
-				beforeValidate: function(user, options) {
-					if (typeof user.email === 'string') {
-						user.email = user.email.toLowerCase()
-					}
-				}
+		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
+			type: DataTypes.VIRTUAL,
+			allowNull: false,
+			vaildate: {
+				len: [7, 100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
-	)
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase()
+				}
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function (){
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt' );
+			}
+		}
+	})
 };
