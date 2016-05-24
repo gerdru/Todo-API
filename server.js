df4e71a865e9d364ef812e802411da16e37d09c2
HP-Pavillion Linux Mint <g.druschke@gmx.de>
2016-05-24 13:56:59 -0400

    Convert function to identify the user

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get('/', function(req, res) {
 // GET /todos?completed=true&q=house
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query; // stores the data queried by the user after the "?"" in the URL
-	var where = {};
+	var where = {
+			userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -49,7 +51,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10); // req.params contains route parameters in the path part of the URL
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			userId: req.user.get('id'),
+			id: todoId
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -83,7 +90,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -106,14 +114,18 @@ app.put('/todos/:id', function(req, res) {
 	var attributes = {};
 
 	if (body.hasOwnProperty('completed')) {
-		validAttributes.completed = body.completed;
+		attributes.completed = body.completed;
 	}
 
 	if (body.hasOwnProperty('description')) {
 		attributes.description = body.description;
 	}
 
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId		
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
@@ -158,7 +170,7 @@ app.post('/users/login', function(req, res) {
 
 
 db.sequelize.sync({
-	force: true
+	force: false
 }).then(function() {
 	app.listen(PORT, function() {
 		console.log('Epxress listening on port' + PORT);
